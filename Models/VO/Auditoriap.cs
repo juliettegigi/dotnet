9cd64ec5f0dc945e2f3a47e826e11681f9af839f
cristian39395110 <102371843+cristian39395110@users.noreply.github.com>
2024-04-24 16:27:33 -0300

    insertauditoriac

---
+++ Models/VO/Auditoriap.cs
@@ -0,0 +1,29 @@
+/*Para los contratos, se deben registrar la fecha de inicio y fecha de
+finalización del mismo (se deben controlar las fechas), el monto de
+alquiler mensual en pesos y un vínculo entre la propiedad inmueble
+y el inquilino. Se debe volver a verificar que el inmueble no esté
+ocupado en esas fechas por otro contrato*/
+
+using System.ComponentModel.DataAnnotations.Schema;
+using InmobiliariaGutierrez.Models.Validacioness;
+
+namespace InmobiliariaGutierrez.Models.VO;
+
+public class Auditoriap
+{
+    public int Id { get; set; }
+    
+   
+    [ForeignKey(nameof(InquilinoId))]
+    public Inquilino InquilinoId { get; set; }
+
+  
+	public DateTime FechaInicio { get; set; }
+
+    public DateTime FechaCancelacion { get; set; }
+	
+
+
+      
+}
+
